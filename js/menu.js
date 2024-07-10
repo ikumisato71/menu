1d4434d50781bde64c3d020c220b75e2eec39915
ikumisato <kinoko7971@gmail.com>
2024-07-10 14:28:24 +0900

    アコーディオンメニューを作成

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+// js-ac要素を取得し変数に格納
+const qa = document.querySelectorAll(".js-ac"); // js-ac要素すべて取得
+// クリック時に発火する関数を作成
+function acToggle() {
+  // js-ac要素の「次の要素」を取得し変数に格納
+  const content = this.nextElementSibling;
+  // js-ac要素の「次の要素」
+  content.classList.toggle("is-open");
+  // js-ac要素自身を変数に格納
+  const qa = this;
+  // js-ac要素にis-openつけ外し
+  qa.classList.toggle("is-open");
+}
+for (let i = 0; i < qa.length; i++) {
+  // for文でjs-acメニューをループ処理
+  qa[i].addEventListener("click", acToggle);
+}
+
+// クリックイベントを登録、acToggle関数を発火
+qa.addEventListener("click", acToggle);
